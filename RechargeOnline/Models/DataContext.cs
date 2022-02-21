eb4103b501dbf5b186df06c942b5671911458697
tranvanlamss <lamt63356@gmail.com>
2022-02-21 08:29:29 +0700

    first commit

---
+++ RechargeOnline/Models/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace RechargeOnline.Models
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext() : base("RechargeOnline")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
